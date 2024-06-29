e5b123a2157a784dd3c2c2a208805d3794203408
Mohammad Yaseen <yaseenyaseenali69@gmail.com>
2024-06-29 15:08:26 +0500

    styling fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -34,14 +34,14 @@ const Home = () => {
 
 
   return (
-    <div className=" w-full min-h-screen">
+    <div className=" w-full min-h-screen absolute top-0 left-0">
       <div className="backdrop-img ">
         <Img src={randomImgPath} />
       </div>
 
 
       <div className="opacity-layer"></div>
-      <div className="flex w-full h-screen flex-col max-xs:pt-16 xs:justify-center items-center">
+      <div className="flex w-full h-[80vh] xs:h-screen flex-col  justify-center items-center">
         <h1 className="text-white font-extrabold text-4xl xs:text-5xl sm:text-7xl font-monstserrat">The Cine Verse</h1>
         <p className="mt-4 mb-10 text-gray-200 font-bold text-center text-sm sm:text-md">Explore Cinematic Worlds: Movies, Series, and Anime at Your Fingertips</p>
         <SearchBox />
